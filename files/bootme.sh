1d5899d74d9aaaf7115052356d378c95ac4019c2
P. Gomersbach <p.gomersbach@rely.nl>
2017-07-07 12:20:18 +0000

    use localhost as monitor address

--- files/bootme.sh
+++ files/bootme.sh
@@ -69,4 +69,4 @@ echo "Preparing modules"
 cp -a /root/role_monitor/spec/fixtures/modules/* $MODULEDIR
 
 echo "Run puppet apply"
-/usr/local/bin/puppet apply -e "include role_monitor" --write-catalog-summary --log_level=err
+FACTER_monitor_address=localhost /usr/local/bin/puppet apply -e "include role_monitor" --write-catalog-summary --log_level=err
